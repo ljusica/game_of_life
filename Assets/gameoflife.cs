d5146eeee235edeaa9b829cb71a2a24d4eb10875
ljusica <jessiicaceder@hotmail.com>
2021-10-20 11:43:51 +0200

    it works!!
    it's not pretty but it works

--- Assets/gameoflife.cs
+++ Assets/gameoflife.cs
@@ -9,7 +9,6 @@ public class GameOfLife : ProcessingLite.GP21
     int numberOfColums;
     int numberOfRows;
     int spawnChancePercentage = 15;
-    int previousGen;
 
     void Start()
     {
@@ -36,20 +35,9 @@ public class GameOfLife : ProcessingLite.GP21
                 cells[x, y] = new GameCell(x * cellSize, y * cellSize, cellSize);
 
                 //Random check to see if it should be alive
-                if (Random.Range(0, 100) < spawnChancePercentage)// && allowed == true)
+                if (Random.Range(0, 100) < spawnChancePercentage)
                 {
                     cells[x, y].alive = true;
-
-                    if (previousGen < 2 || previousGen > 3)
-                    {
-                        cells[x, y].alive = false;
-                    }
-
-                    if (cells[x, y].alive == false && previousGen == 3)
-                    {
-                        cells[x, y].alive = true;
-                    }
-
                 }
             }
         }
@@ -60,88 +48,95 @@ public class GameOfLife : ProcessingLite.GP21
         //Clear screen
         Background(0);
 
+        for (int y = 1; y < numberOfRows-1; ++y)
+        {
+            for (int x = 1; x < numberOfColums-1; ++x)
+            {
+                cells[x, y].previousGen = CalculateNeighbours(x, y);
+            }
+        }
+
         for (int y = 0; y < numberOfRows; ++y)
         {
             for (int x = 0; x < numberOfColums; ++x)
             {
-                previousGen = CalculateNeighbours();
+                if (cells[x, y].previousGen < 2 || cells[x, y].previousGen > 3)
+                {
+                    cells[x, y].alive = false;
+                }
+
+                if (cells[x, y].alive == false && cells[x, y].previousGen == 3)
+                {
+                    cells[x, y].alive = true;
+                }
             }
         }
-
-        //TODO: update buffer
 
         //Draw all cells.
         for (int y = 0; y < numberOfRows; ++y)
         {
             for (int x = 0; x < numberOfColums; ++x)
             {
-                //Draw current cell
                 cells[x, y].Draw();
             }
         }
     }
 
-    public int CalculateNeighbours()
+    public int CalculateNeighbours(int x, int y)
     {
         int neighbours = 0;
 
-        for (int y = 1; y < numberOfRows - 1; ++y)
+        if (cells[x - 1, y].alive)
         {
-            for (int x = 1; x < numberOfColums - 1; ++x)
-            {
-
-                if (cells[x - 1, y].alive)
-                {
-                    neighbours++;
-                }
-
-                if (cells[x, y - 1].alive)
-                {
-                    neighbours++;
-                }
-
-                if (cells[x + 1, y].alive)
-                {
-                    neighbours++;
-                }
-
-                if (cells[x, y + 1].alive)
-                {
-                    neighbours++;
-                }
-
-                if (cells[x - 1, y - 1].alive)
-                {
-                    neighbours++;
-                }
-
-                if (cells[x + 1, y + 1].alive)
-                {
-                    neighbours++;
-                }
-
-                if (cells[x + 1, y - 1].alive)
-                {
-                    neighbours++;
-                }
-
-                if (cells[x - 1, y + 1].alive)
-                {
-                    neighbours++;
-                }
-            }
+            neighbours++;
         }
+
+        if (cells[x, y - 1].alive)
+        {
+            neighbours++;
+        }
+
+        if (cells[x + 1, y].alive)
+        {
+            neighbours++;
+        }
+
+        if (cells[x, y + 1].alive)
+        {
+            neighbours++;
+        }
+
+        if (cells[x - 1, y - 1].alive)
+        {
+            neighbours++;
+        }
+
+        if (cells[x + 1, y + 1].alive)
+        {
+            neighbours++;
+        }
+
+        if (cells[x + 1, y - 1].alive)
+        {
+            neighbours++;
+        }
+
+        if (cells[x - 1, y + 1].alive)
+        {
+            neighbours++;
+        }
+
         return neighbours;
     }
 
 }
-
 
 //You will probebly need to keep track of more things in this class
 public class GameCell : ProcessingLite.GP21
 {
     float x, y; //Keep track of our position
     float size; //our size
+    public int previousGen;
 
     //Keep track if we are alive
     public bool alive = false;
@@ -169,39 +164,4 @@ public class GameCell : ProcessingLite.GP21
             //new Vector existing = [this.x, this.y]
         }
     }
-
-    //public class Calculate : ProcessingLite.GP21 ----- Gör till funktion
-    //{
-
-    //	/* for varje prick i grid
-    //	 * kolla position + lagra i lista (8 närmsta)
-    //	 */
-    //}
-
-    //public class Validera : ProcessingLite.GP21 ---- Också funktion
-    //{ 
-    ///*		 
-    // if this.GameCell == alive && this.GameCell < 2 alive neighbours
-    // {
-    //	cell == död(underpopulation)
-    // }
-
-    // OM cell == alive && cell == 2 alive neigbours || cell == alive && cell == 3 alive neighbours
-    //{
-    //	cell == alive (next gen)
-    //	allowed == true;
-    // }
-
-    // OM cell == alive && cell == > 3 alive neighbours
-    // {
-    //	cell == död (overpopulation)
-    // }
-
-    // OM cell == död && cell == 3 alive neighbours
-    // {
-    //	cell == alive (reproduction)
-    //	allowed == true;
-    // }
-    // */
-    //}
 }
